2202c506cd7b13c611a2baf0a3514046dc38b859
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2023-08-10 08:57:54 +0200

    _

--- plugin.cpp
+++ plugin.cpp
@@ -1,8 +1,92 @@
 #include "logger.h"
 
+bool PostPostLoaded = false;
+RE::UI* ui = nullptr;
+bool gameIsPaused = false;
+float duration = 8.0f;
+float remaining = 8.0f;
+
+
+class OurEventSink : public RE::BSTEventSink<RE::MenuOpenCloseEvent>,
+                     public RE::BSTEventSink<RE::TESEquipEvent>,
+                     public RE::BSTEventSink<RE::TESTrackedStatsEvent> {
+    OurEventSink() = default;
+    OurEventSink(const OurEventSink&) = delete;
+    OurEventSink(OurEventSink&&) = delete;
+    OurEventSink& operator=(const OurEventSink&) = delete;
+    OurEventSink& operator=(OurEventSink&&) = delete;
+
+public:
+    static OurEventSink* GetSingleton() {
+        static OurEventSink singleton;
+        return &singleton;
+    }
+
+    RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* event,
+                                          RE::BSTEventSource<RE::MenuOpenCloseEvent>*) {
+        if (!event) return RE::BSEventNotifyControl::kContinue;
+        
+        return RE::BSEventNotifyControl::kContinue;
+    }
+
+    RE::BSEventNotifyControl ProcessEvent(const RE::TESEquipEvent* event, RE::BSTEventSource<RE::TESEquipEvent>*) {
+        if (!event) return RE::BSEventNotifyControl::kContinue;
+        auto obj = RE::TESForm::LookupByID(event->baseObject);
+        if (obj && (std::string_view)obj->GetName() == "Iron Lantern") {
+            //auto asd = RE::SkyrimVM::GetSingleton();
+            //logger::info("{}", asd->queuedOnUpdateEvents.size());
+            //auto lmao = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+            //lmao->Update(100000000.0f);
+            //logger::info("{}", asd->queuedOnUpdateEvents.size());
+            /*for (auto& i : asd->queuedOnUpdateEvents) {
+				logger::info("updateType: {}", (int)i.get()->updateType);
+                logger::info("timeToSendEvent: {}", i.get()->timeToSendEvent);
+                logger::info("updateTime: {}", i.get()->updateTime);
+                logger::info("handle: {}", (int)i.get()->handle);
+			}*/
+            //gameIsPaused = true;
+            
+        }
+        if (gameIsPaused) logger::info("asdasd");
+
+
+        return RE::BSEventNotifyControl::kContinue;
+    }
+
+    RE::BSEventNotifyControl ProcessEvent(const RE::TESTrackedStatsEvent* event,
+                                          RE::BSTEventSource<RE::TESTrackedStatsEvent>*) {
+        if (!event) return RE::BSEventNotifyControl::kContinue;
+        logger::info("{} {} {}", event->stat, event->value, event->pad0C);
+        SKSE::Impl::EventFilter<std::string>::RegistrationMap<bool,RE::TESGlobal> asd;
+
+
+
+        return RE::BSEventNotifyControl::kContinue;
+    }
+
+};
+
+void OnMessage(SKSE::MessagingInterface::Message* message) {
+    switch (message->type) {
+        case SKSE::MessagingInterface::kInputLoaded:
+            break;
+        case SKSE::MessagingInterface::kPostPostLoad:
+            if (!PostPostLoaded) {
+                ui = RE::UI::GetSingleton();
+                ui->AddEventSink<RE::MenuOpenCloseEvent>(OurEventSink::GetSingleton());
+                RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESEquipEvent>(OurEventSink::GetSingleton());
+                RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESTrackedStatsEvent>(OurEventSink::GetSingleton());
+                PostPostLoaded = true;
+            }
+            break;
+    }
+};
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
 
     SetupLog();
+    spdlog::set_pattern("%v");
     SKSE::Init(skse);
+    SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
     return true;
 }
